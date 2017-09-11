dd4a6816086342d3d0f86b51c443c79a88265024
John Suen <johnnie.suen@gmail.com>
2017-09-11 20:10:27 +1000

    Add project files.

---
+++ WindowsFormsApplication5/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsApplication5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(12, 32);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(55, 13);
+            this.lblOutput.TabIndex = 1;
+            this.lblOutput.Text = "{{Output}}";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 9);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 2;
+            this.txtInput.Text = "{{Input}}";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.TextBox txtInput;
+    }
+}
+
---
+++ WindowsFormsApplication5/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Data;
+using System.Messaging;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication5
+{
+    public partial class Form1 : Form
+    {
+        private readonly MessageQueue _messageQueue = new MessageQueue();
+        private readonly DataTable _dataTable= new DataTable("Employee");
+
+        public Form1(string messageQueuePath)
+        {
+            InitializeComponent();
+
+            //How to test the following are being executed
+            _messageQueue.ReceiveCompleted += new System.Messaging.ReceiveCompletedEventHandler(_messageQueue_ReceiveCompleted);
+            _messageQueue.Path = messageQueuePath;
+            _messageQueue.BeginReceive(new TimeSpan(0, 0, 3));
+
+            _dataTable.Columns.Add("FirstName", typeof(string));
+            _dataTable.Columns.Add("LastName", typeof(string));
+            _dataTable.Columns.Add("Department", typeof(string));
+            _dataTable.Columns.Add("JobTitle", typeof(string));
+
+        }
+
+        public string Output
+        {
+            //Should you use setter/getter to set and get or methods?
+            get { return lblOutput.Text; }
+            set { lblOutput.Text = value; }
+        }
+
+        public string GetOuput()
+        {
+            //Should you use methods to interact with the UI control or setter/getter?
+            return lblOutput.Text;
+        }
+
+        public void SetOuput(string s)
+        {
+            //Should you use methods to interact with the UI control or setter/getter?
+            lblOutput.Text = s;
+        }
+
+        private void _messageQueue_ReceiveCompleted(object sender, ReceiveCompletedEventArgs e)
+        {
+            //How to test this?
+            MessageQueue mq = (MessageQueue)sender;
+            mq.Formatter = new XmlMessageFormatter(new Type[]
+            {
+                typeof(Employee)
+            });
+
+            System.Messaging.Message m = mq.EndReceive(e.AsyncResult);
+
+            switch (m.Body.GetType().ToString())
+            {
+                case "Employee":
+                    Process((Employee)m.Body);
+                    break;
+            }
+        }
+
+
+        private void Process(Employee employee)
+        {
+            //How to test this?
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Do you need to test this? As it's external dependency        
+            _dataTable.Rows.Add(txtInput.Text);
+        }
+    }
+
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+        public string JobTitle { get; set; }
+    }
+}
